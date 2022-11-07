f47958dc606cc8bc397e0d6e1c4333d181f7a5b7
DaniKPosada <d.posada1044@gmail.com>
2022-11-07 17:33:37 -0500

    escritura y limpia al actualizar

--- calc.js
+++ calc.js
@@ -1,22 +1,41 @@
 const botonNumeros=document.getElementsByName('data-number');
 const botonOpera=document.getElementsByName('data-opera');
 const botonIgual=document.getElementsByName('data-igual');
-const botonBorrar=document.getElementsByName('data-borrar')[0];
-const botonCancela=document.getElementsByName('cancelaTodo')[0];
-const botonVista=document.getElementsByName('vista')[0];
+const botonBorrar=document.getElementsByName('data-borrar');
+const botonCancela=document.getElementsByName('cancelaTodo');
+const botonVista=document.getElementsByName('vista');
 var resultado= document.getElementById('resultado');
-
+var opActual='';
+var opAnt='';
+var operacion=undefined;
 botonNumeros.forEach(function(boton){
     boton.addEventListener('click',function(){
     agregarNumero(boton.innerText);
     })
-})
+});
 botonOpera.forEach(function(boton){
     boton.addEventListener('click', function(){
         selectOperacion(boton.innerText);
     })
-})
+});
 botonIgual.addEventListener('click',function(){
     calcular();
     actualizarDisplay();
-})
+});
+botonBorrar.addEventListener('click',function(){
+    clear();
+    actualizarDisplay();
+});
+function agregarNumero(num){
+    opActual=opActual.toString()+num.toString();
+    actualizarDisplay();
+};
+function clear(){
+    opActual='';
+    opAnt='';
+    operacion=undefined;
+};
+function actualizarDisplay(){
+    resultado.value=opActual;
+};
+clear();
